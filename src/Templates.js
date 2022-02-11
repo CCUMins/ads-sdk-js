41e05bffef3be0b76675bb68fd107af4c609e4fc
yoredut <yoryi.pacheco@gmail.com>
2022-02-11 19:26:07 -0300

    fixes

--- src/Templates.js
+++ src/Templates.js
@@ -16,7 +16,7 @@ const template = (isLandscape, media, parsedAction, token) => {
       <div id='ccu-ads-player-banner' class='ads-style-w-full ads-style-items-center ads-style-justify-center ads-style-bg-white' style='flex: 1;'>
         <div class='ads-style-w-full ads-style-h-full ads-style-items-center ads-style-justify-center'>
           <div class='ads-style-w-full ads-style-px-1 ads-style-flex-row ads-style-justify-start' style='height: 3.5rem; border-bottom: 1px solid #799499;'>
-            <span class='ads-style-font-sm ads-style-text-white ads-style-text-center ads-style-bg-light-blue' style='padding: 5px 10px; border-radius: 1000px;'>Ad</span>
+            <span class='ads-style-font-sm ads-style-text-white ads-style-text-center ads-style-bg-blue' style='padding: 5px 10px; border-radius: 1000px;'>BTC-Ads</span>
             <span class='ads-style-font-xs ads-style-ml-1 ads-style-font-bold'>${parsedAction}</span>
             <div id='ccu-ads-close-ad-button-mobile' style='width: 1.5rem; height: 1.5rem; margin-left: auto;'>
               <img class='ads-style-w-full' src='https://ccu-public.s3.us-east-2.amazonaws.com/ads-sdk-assets/close_white_mb.svg'/>
@@ -33,12 +33,12 @@ const template = (isLandscape, media, parsedAction, token) => {
               </button>
             </div>
           </div>
-          <div class='ads-style-items-center ads-style-justify-center ads-style-flex-row ads-style-bg-gray ${isMobile() ? 'ads-style-mb-1' : ''}' style='height: 2.8rem; border-radius: 1.5rem; padding-right: 0.5rem;'>
+          <div class='ads-style-items-center ads-style-justify-center ads-style-flex-row ads-style-bg-blue ${isMobile() ? 'ads-style-mb-1' : ''}' style='height: 2.8rem; border-radius: 1.5rem; padding-right: 0.5rem;'>
             <div>
               <img src='https://ccu-public.s3.us-east-2.amazonaws.com/ads-sdk-assets/bitcoin.svg' style='width: 34px; height: 34px;' />
               <svg><circle id='ccu-countdown' r='18' cx='20' cy='20'></circle></svg>
             </div>
-            <span class=' ads-style-font-sm ml-1'><span class='ads-style-font-bold'>${numberFormat(media.reward, 0)} SATs</span> will be yours in <span id='ccu-ads-player-timer'></span> seconds</span>
+            <span class=' ads-style-font-sm ml-1 ads-style-text-white'><span class='ads-style-font-bold'>${numberFormat(media.reward, 0)} SATs</span> will be yours in <span id='ccu-ads-player-timer'></span> seconds</span>
           </div>
         </div>
       </div>
@@ -86,7 +86,7 @@ const template = (isLandscape, media, parsedAction, token) => {
            <div class='ads-style-fixed ads-style-w-full ads-style-h-full ads-style-items-center ads-style-justify-center ads-style-bg-black' style='z-index: 999999999;'>
             <div class='ads-style-w-full ads-style-h-full ads-style-items-center ads-style-justify-between'>
               <div id='ccu-ads-player-container-id' class='ads-style-w-full ads-style-flex-1 ads-style-items-center ads-style-justify-center'>
-                <div class='ads-style-absolute ads-style-cursor-pointer' id='ccu-ads-close-ad-button' style='width: 1.5rem; height: 1.5rem; top: 1.5rem; right: 1.5rem;' >
+                <div class='ads-style-absolute ads-style-cursor-pointer' id='ccu-ads-close-ad-button' style='width: 1.5rem; height: 1.5rem; top: 1.5rem; right: 1.5rem; z-index: 3;' >
                   <img src='https://ccu-public.s3.us-east-2.amazonaws.com/ads-sdk-assets/close_white.svg'/>
                 </div>
                 <!--<video id='ccu-ads-player-video-id' />-->
@@ -94,7 +94,7 @@ const template = (isLandscape, media, parsedAction, token) => {
               <div id='ccu-ads-player-banner' class='ads-style-w-full ads-style-max-w-7xl ads-style-items-center ads-style-justify-center ads-style-bg-white' style='height: 100px; border-radius: 2rem 2rem 0 0; padding: 0 1rem'>
                 <div class='ads-style-w-full ads-style-h-full ads-style-items-center ads-style-justify-center ads-style-flex-row ads-style-justify-between'>
                   <div class='ads-style-flex-row ads-style-justify-start ads-style-absolute' style='top: -1rem'>
-                    <span class='${isMobile() ? 'ads-style-font-xs' : 'ads-style-font-sm'} ads-style-text-white ads-style-text-center ads-style-bg-light-blue' style='padding: 5px 10px; border-radius: 1000px;'>BTC-Ads</span>
+                    <span class='${isMobile() ? 'ads-style-font-xs' : 'ads-style-font-sm'} ads-style-text-white ads-style-text-center ads-style-bg-blue' style='padding: 5px 10px; border-radius: 1000px;'>BTC-Ads</span>
                   </div>
                   <div class='ads-style-h-full ads-style-items-center ads-style-justify-center ads-style-flex-row'>
                     <div class='ads-style-flex-row ads-style-ml-2 ads-style-w-full'>
@@ -109,12 +109,12 @@ const template = (isLandscape, media, parsedAction, token) => {
                       </button>
                     </div>
                   </div>
-                  <div class='ads-style-items-center ads-style-justify-center ads-style-flex-row ads-style-bg-gray' style='height: 40px; border-radius: 24px; padding-right: 8px;'>
+                  <div class='ads-style-items-center ads-style-justify-center ads-style-flex-row ads-style-bg-blue' style='height: 40px; border-radius: 24px; padding-right: 8px;'>
                     <div>
                       <img src='https://ccu-public.s3.us-east-2.amazonaws.com/ads-sdk-assets/bitcoin.svg' style='width: 40px; height: 40;' />
                       <svg><circle id='ccu-countdown' r='18' cx='20' cy='20'></circle></svg>
                     </div>
-                    <span class='${isMobile() ? 'ads-style-font-xs' : 'ads-style-font-sm'} ads-style-ml-1'><span class='ads-style-font-bold'>${numberFormat(media.reward, 0)} SATs</span> will be yours in <span id='ccu-ads-player-timer'></span> seconds</span>
+                    <span class='${isMobile() ? 'ads-style-font-xs' : 'ads-style-font-sm'} ads-style-ml-1 text-white'><span class='ads-style-font-bold'>${numberFormat(media.reward, 0)} SATs</span> will be yours in <span id='ccu-ads-player-timer'></span> seconds</span>
                   </div>
                 </div>
               </div>
